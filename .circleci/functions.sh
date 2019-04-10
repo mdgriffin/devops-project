ab5991921d5059c1be3fd6b9e55003909a40a4a6
Michael Griffin <mdgriffin064@gmail.com>
2019-04-10 12:10:40 +0100

    Sending Grafana Password and Service IP address to Slack

--- .circleci/functions.sh
+++ .circleci/functions.sh
@@ -3,3 +3,9 @@
 function send_slack_message {
     curl -d "{\"text\":\"${1}\"}" -H "Content-Type: application/json" -X POST $SLACK_WEB_HOOK
 }
+
+function pipe_slack_message {
+    while read data; do
+      send_slack_message "$data"
+    done
+}
